0d6bdfbc8b81783de1988512b5366b1112fc06a3
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2018-11-15 23:40:13 +0200

    Add DesignTimeDbContextFactory

---
+++ src/PoolIt.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,29 @@
+namespace PoolIt.Data
+{
+    using System.IO;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.EntityFrameworkCore.Diagnostics;
+    using Microsoft.Extensions.Configuration;
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<PoolItDbContext>
+    {
+        public PoolItDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<PoolItDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            builder.ConfigureWarnings(w => w.Throw(RelationalEventId.QueryClientEvaluationWarning));
+
+            return new PoolItDbContext(builder.Options);
+        }
+    }
+}
