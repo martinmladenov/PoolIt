a47422d4868b64b1630680b81969b2e6e6819b6d
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2018-11-18 09:34:52 +0200

    Implement model creation

---
+++ src/PoolIt.Web/Controllers/ModelsController.cs
@@ -0,0 +1,57 @@
+namespace PoolIt.Web.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using Services.Contracts;
+    using Services.Models;
+
+    public class ModelsController : Controller
+    {
+        private readonly IManufacturersService manufacturersService;
+        private readonly IModelsService modelsService;
+
+        public ModelsController(IManufacturersService manufacturersService, IModelsService modelsService)
+        {
+            this.manufacturersService = manufacturersService;
+            this.modelsService = modelsService;
+        }
+
+        [Authorize]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create(CarModelCreateBindingModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View();
+            }
+
+            var manufacturer = await this.manufacturersService.Get(model.Manufacturer);
+
+            if (manufacturer == null)
+            {
+                manufacturer = new CarManufacturerServiceModel
+                {
+                    Name = model.Manufacturer
+                };
+            }
+
+            var carModel = new CarModelServiceModel
+            {
+                Manufacturer = manufacturer,
+                Model = model.CarModel
+            };
+
+            await this.modelsService.Create(carModel);
+
+            return this.RedirectToAction("Index", "Home");
+        }
+    }
+}
