c84b2370ff0cb8fa12f5a5b96b209a91fe795be4
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2018-11-25 21:45:53 +0200

    Move date format to GlobalConstants

--- src/PoolIt.Infrastructure/GlobalConstants.cs
+++ src/PoolIt.Infrastructure/GlobalConstants.cs
@@ -3,5 +3,7 @@ namespace PoolIt.Infrastructure
     public static class GlobalConstants
     {
         public const string AdminRoleName = "Administrator";
+
+        public const string DateFormat = "dd/MM/yyyy HH:mm";
     }
 }
