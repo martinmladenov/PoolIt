3a8bce38e1d4275dd2dde9305e76073ad24afd4d
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2018-11-29 10:46:40 +0200

    Add OAuth with GitHub

---
+++ src/PoolIt.Web/Models/UserGitHubRegisterBindingModel.cs
@@ -0,0 +1,22 @@
+namespace PoolIt.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class UserGitHubRegisterBindingModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+    }
+}
