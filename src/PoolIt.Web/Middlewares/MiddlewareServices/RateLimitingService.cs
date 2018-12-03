3a740ae6a1616b412b30fcaed909bc7ec4b9f054
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2018-12-03 22:18:25 +0200

    Implement RateLimitingMiddleware

---
+++ src/PoolIt.Web/Middlewares/MiddlewareServices/RateLimitingService.cs
@@ -0,0 +1,97 @@
+namespace PoolIt.Web.Middlewares.MiddlewareServices
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using Contracts;
+
+    public class RateLimitingService : IRateLimitingService
+    {
+        private const int AllowedRequestsInTimeInterval = 50;
+        private const int TimeIntervalInMinutes = 1;
+
+        private readonly ConcurrentDictionary<string, ClientInfo> clients;
+
+        public RateLimitingService()
+        {
+            this.clients = new ConcurrentDictionary<string, ClientInfo>();
+        }
+
+        public bool RegisterClientRequest(string clientIp)
+        {
+            var client = this.clients.GetOrAdd(clientIp, s => new ClientInfo());
+
+            if (client.IsLocked)
+            {
+                return true;
+            }
+
+            lock (client.LockObj)
+            {
+                var requestsQueue = client.Requests;
+
+                requestsQueue.Enqueue(DateTime.Now.AddMinutes(TimeIntervalInMinutes));
+                RemoveExpired(requestsQueue);
+
+                if (requestsQueue.Count > AllowedRequestsInTimeInterval)
+                {
+                    client.IsLocked = true;
+                }
+            }
+
+            return client.IsLocked;
+        }
+
+        public bool IsClientLocked(string clientIp)
+        {
+            var clientExists = this.clients.TryGetValue(clientIp, out var client);
+
+            if (!clientExists)
+            {
+                return false;
+            }
+
+            return client.IsLocked;
+        }
+
+        public void UnlockClient(string clientIp)
+        {
+            var clientExists = this.clients.TryGetValue(clientIp, out var client);
+
+            if (!clientExists)
+            {
+                return;
+            }
+
+            lock (client.LockObj)
+            {
+                client.Requests.Clear();
+                client.IsLocked = false;
+            }
+        }
+
+        private static void RemoveExpired(Queue<DateTime> queue)
+        {
+            var currTime = DateTime.Now;
+
+            while (queue.Count > 0 && queue.Peek() <= currTime)
+            {
+                queue.Dequeue();
+            }
+        }
+
+        private class ClientInfo
+        {
+            public ClientInfo()
+            {
+                this.Requests = new Queue<DateTime>();
+                this.IsLocked = false;
+                this.LockObj = new object();
+            }
+
+            public Queue<DateTime> Requests { get; }
+            public bool IsLocked { get; set; }
+            public object LockObj { get; }
+        }
+    }
+}
